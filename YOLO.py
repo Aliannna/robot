2b5a9d1251c8e85d241b46af32c0bcaa5a520a89
Aliannna <bookliger707@gmail.com>
2022-08-02 11:48:52 -0700

    Rename file; edit YOLO code

--- YOLO.py
+++ YOLO.py
@@ -62,23 +62,6 @@ for label, confidence, bbox in detections:
                     class_colors[label], 2)
 cv2_imshow(image)
 
-# function to convert the JavaScript object into an OpenCV image
-def js_to_image(js_reply):
-  """
-  Params:
-          js_reply: JavaScript object containing image from webcam
-  Returns:
-          img: OpenCV BGR image
-  """
-  # decode base64 image
-  image_bytes = b64decode(js_reply.split(',')[1])
-  # convert bytes to numpy array
-  jpg_as_np = np.frombuffer(image_bytes, dtype=np.uint8)
-  # decode numpy array into OpenCV BGR image
-  img = cv2.imdecode(jpg_as_np, flags=1)
-
-  return img
-
 # function to convert OpenCV Rectangle bounding box image into base64 byte string to be overlayed on video stream
 def bbox_to_bytes(bbox_array):
   """
@@ -96,40 +79,6 @@ def bbox_to_bytes(bbox_array):
   bbox_bytes = 'data:image/png;base64,{}'.format((str(b64encode(iobuf.getvalue()), 'utf-8')))
 
   return bbox_bytes
-
-  def take_photo(filename='photo.jpg', quality=0.8):
-  js = Javascript('''
-    async function takePhoto(quality) {
-      const div = document.createElement('div');
-      const capture = document.createElement('button');
-      capture.textContent = 'Capture';
-      div.appendChild(capture);
-
-      const video = document.createElement('video');
-      video.style.display = 'block';
-      const stream = await navigator.mediaDevices.getUserMedia({video: true});
-
-      document.body.appendChild(div);
-      div.appendChild(video);
-      video.srcObject = stream;
-      await video.play();
-
-      // Resize the output to fit the video element.
-      google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
-
-      // Wait for Capture to be clicked.
-      await new Promise((resolve) => capture.onclick = resolve);
-
-      const canvas = document.createElement('canvas');
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      canvas.getContext('2d').drawImage(video, 0, 0);
-      stream.getVideoTracks()[0].stop();
-      div.remove();
-      return canvas.toDataURL('image/jpeg', quality);
-    }
-    ''')
-  display(js)
 
   # get photo data
   data = eval_js('takePhoto({})'.format(quality))
@@ -163,138 +112,39 @@ except Exception as err:
   # grant the page permission to access it.
   print(str(err))
 
-  # JavaScript to properly create our live video stream using our webcam as input
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# allow the camera to warm up
+time.sleep(0.1)
+
 def video_stream():
-  js = Javascript('''
-    var video;
-    var div = null;
-    var stream;
-    var captureCanvas;
-    var imgElement;
-    var labelElement;
-    
-    var pendingResolve = null;
-    var shutdown = false;
-    
-    function removeDom() {
-       stream.getVideoTracks()[0].stop();
-       video.remove();
-       div.remove();
-       video = null;
-       div = null;
-       stream = null;
-       imgElement = null;
-       captureCanvas = null;
-       labelElement = null;
-    }
-    
-    function onAnimationFrame() {
-      if (!shutdown) {
-        window.requestAnimationFrame(onAnimationFrame);
-      }
-      if (pendingResolve) {
-        var result = "";
-        if (!shutdown) {
-          captureCanvas.getContext('2d').drawImage(video, 0, 0, 640, 480);
-          result = captureCanvas.toDataURL('image/jpeg', 0.8)
-        }
-        var lp = pendingResolve;
-        pendingResolve = null;
-        lp(result);
-      }
-    }
-    
-    async function createDom() {
-      if (div !== null) {
-        return stream;
-      }
+  for frame in camera.capture_continuous(rawCapture, format="bgr",
+                                         use_video_port=True):
 
-      div = document.createElement('div');
-      div.style.border = '2px solid black';
-      div.style.padding = '3px';
-      div.style.width = '100%';
-      div.style.maxWidth = '600px';
-      document.body.appendChild(div);
-      
-      const modelOut = document.createElement('div');
-      modelOut.innerHTML = "<span>Status:</span>";
-      labelElement = document.createElement('span');
-      labelElement.innerText = 'No data';
-      labelElement.style.fontWeight = 'bold';
-      modelOut.appendChild(labelElement);
-      div.appendChild(modelOut);
-           
-      video = document.createElement('video');
-      video.style.display = 'block';
-      video.width = div.clientWidth - 6;
-      video.setAttribute('playsinline', '');
-      video.onclick = () => { shutdown = true; };
-      stream = await navigator.mediaDevices.getUserMedia(
-          {video: { facingMode: "environment"}});
-      div.appendChild(video);
+      # grab the raw NumPy array representing the image, then 
+      # initialize the timestamp and occupied/unoccupied text
+      image = frame.array
 
-      imgElement = document.createElement('img');
-      imgElement.style.position = 'absolute';
-      imgElement.style.zIndex = 1;
-      imgElement.onclick = () => { shutdown = true; };
-      div.appendChild(imgElement);
-      
-      const instruction = document.createElement('div');
-      instruction.innerHTML = 
-          '<span style="color: red; font-weight: bold;">' +
-          'When finished, click here or on the video to stop this demo</span>';
-      div.appendChild(instruction);
-      instruction.onclick = () => { shutdown = true; };
-      
-      video.srcObject = stream;
-      await video.play();
+      result = predictor.detect(image)
 
-      captureCanvas = document.createElement('canvas');
-      captureCanvas.width = 640; //video.videoWidth;
-      captureCanvas.height = 480; //video.videoHeight;
-      window.requestAnimationFrame(onAnimationFrame);
-      
-      return stream;
-    }
-    async function stream_frame(label, imgData) {
-      if (shutdown) {
-        removeDom();
-        shutdown = false;
-        return '';
-      }
+      for obj in result:
+          logger.info('coordinates: {} {}. class: "{}". confidence: {:.2f}'.
+                      format(obj[0], obj[1], obj[3], obj[2]))
 
-      var preCreate = Date.now();
-      stream = await createDom();
-      
-      var preShow = Date.now();
-      if (label != "") {
-        labelElement.innerHTML = label;
-      }
-            
-      if (imgData != "") {
-        var videoRect = video.getClientRects()[0];
-        imgElement.style.top = videoRect.top + "px";
-        imgElement.style.left = videoRect.left + "px";
-        imgElement.style.width = videoRect.width + "px";
-        imgElement.style.height = videoRect.height + "px";
-        imgElement.src = imgData;
-      }
-      
-      var preCapture = Date.now();
-      var result = await new Promise(function(resolve, reject) {
-        pendingResolve = resolve;
-      });
-      shutdown = false;
-      
-      return {'create': preShow - preCreate, 
-              'show': preCapture - preShow, 
-              'capture': Date.now() - preCapture,
-              'img': result};
-    }
-    ''')
+          cv2.rectangle(image, obj[0], obj[1], (0, 255, 0), 2)
+          cv2.putText(image, '{}: {:.2f}'.format(obj[3], obj[2]),
+                      (obj[0][0], obj[0][1] - 5),
+                      cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
 
-  display(js)
-  
+
+    # show the frame
+    cv2.imshow("Stream", image)
+    key = cv2.waitKey(1) & 0xFF
+
 def video_frame(label, bbox):
   data = eval_js('stream_frame("{}", "{}")'.format(label, bbox))
   return data
