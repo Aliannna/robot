3ac62b97a2789c258594b86527e74ac6a1d69859
Aliannna <bookliger707@gmail.com>
2022-08-03 11:08:37 -0700

    Troubleshoot importing Darknet

--- YOLO.py
+++ YOLO.py
@@ -10,20 +10,19 @@ import io
 import html
 import time
 import matplotlib.pyplot as plt
-%matplotlib inline
 
-!git clone https://github.com/AlexeyAB/darknet
+git clone https://github.com/AlexeyAB/darknet
 
-%cd darknet
-!sed -i 's/OPENCV=0/OPENCV=1/' Makefile
-!sed -i 's/GPU=0/GPU=1/' Makefile
-!sed -i 's/CUDNN=0/CUDNN=1/' Makefile
-!sed -i 's/CUDNN_HALF=0/CUDNN_HALF=1/' Makefile
-!sed -i 's/LIBSO=0/LIBSO=1/' Makefile
+cd darknet
+sed -i 's/OPENCV=0/OPENCV=1/' Makefile
+sed -i 's/GPU=0/GPU=1/' Makefile
+sed -i 's/CUDNN=0/CUDNN=1/' Makefile
+sed -i 's/CUDNN_HALF=0/CUDNN_HALF=1/' Makefile
+sed -i 's/LIBSO=0/LIBSO=1/' Makefile
 
-!make
+make
 
-!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1V3vsIaxAlGWvK4Aar9bAiK5U0QFttKwq' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1V3vsIaxAlGWvK4Aar9bAiK5U0QFttKwq" -O yolov4-csp.weights && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1V3vsIaxAlGWvK4Aar9bAiK5U0QFttKwq' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1V3vsIaxAlGWvK4Aar9bAiK5U0QFttKwq" -O yolov4-csp.weights && rm -rf /tmp/cookies.txt
 
 # import darknet functions to perform object detections
 from darknet import *
@@ -81,54 +80,18 @@ for frame in camera.capture_continuous(rawCapture, format="bgr",
 
   detections, width_ratio, height_ratio = darknet_helper(image, width, height)
 
-  for obj in result:
-    logger.info('coordinates: {} {}. class: "{}". confidence: {:.2f}'.
-                format(obj[0], obj[1], obj[3], obj[2]))
+  for label, confidence, bbox in detections:
+  left, top, right, bottom = bbox2points(bbox)
+  left, top, right, bottom = int(left * width_ratio), int(top * height_ratio), int(right * width_ratio), int(bottom * height_ratio)
+  cv2.rectangle(image, (left, top), (right, bottom), class_colors[label], 2)
+  cv2.putText(image, "{} [{:.2f}]".format(label, float(confidence)),
+                    (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    class_colors[label], 2)
 
-    cv2.rectangle(image, obj[0], obj[1], (0, 255, 0), 2)
-    cv2.putText(image, '{}: {:.2f}'.format(obj[3], obj[2]),
-                (obj[0][0], obj[0][1] - 5),
-                cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
 
 
   # show the frame
   cv2.imshow("Stream", image)
   key = cv2.waitKey(1) & 0xFF
-
-# start streaming video from webcam
-video_stream()
-# label for video
-label_html = 'Capturing...'
-# initialze bounding box to empty
-bbox = ''
-count = 0 
-while True:
-    js_reply = video_frame(label_html, bbox)
-    if not js_reply:
-        break
-
-    # convert JS response to OpenCV Image
-    frame = js_to_image(js_reply["img"])
-
-    # create transparent overlay for bounding box
-    bbox_array = np.zeros([480,640,4], dtype=np.uint8)
-
-    # call our darknet helper on video frame
-    detections, width_ratio, height_ratio = darknet_helper(frame, width, height)
-
-    # loop through detections and draw them on transparent overlay image
-    for label, confidence, bbox in detections:
-      left, top, right, bottom = bbox2points(bbox)
-      left, top, right, bottom = int(left * width_ratio), int(top * height_ratio), int(right * width_ratio), int(bottom * height_ratio)
-      bbox_array = cv2.rectangle(bbox_array, (left, top), (right, bottom), class_colors[label], 2)
-      bbox_array = cv2.putText(bbox_array, "{} [{:.2f}]".format(label, float(confidence)),
-                        (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                        class_colors[label], 2)
-
-    bbox_array[:,:,3] = (bbox_array.max(axis = 2) > 0 ).astype(int) * 255
-    # convert overlay of bbox into bytes
-    bbox_bytes = bbox_to_bytes(bbox_array)
-    # update bbox so next frame gets new overlay
-    bbox = bbox_bytes
 
     
