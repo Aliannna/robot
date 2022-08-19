5b3c3dd60f21204106a5b979f7fa0ba97e58303c
Aliannna <bookliger707@gmail.com>
2022-08-19 13:43:28 -0700

    whoopsie

--- test.py
+++ test.py
@@ -8,7 +8,41 @@
 # sleep(2)
 # camera.capture('foo.jpg')
 
-# import time, board,22!4
+# import time, board, busio, adafruit_vl53l0x
+
+# import numpy as np
+# from driver import Driver
+
+# # Initialize I2C bus and sensor.
+# i2c = busio.I2C(board.SCL, board.SDA)
+# vl53 = adafruit_vl53l0x.VL53L0X(i2c)
+
+# A1 = 27
+# A2 = 4
+# APWM = 12
+
+# B1 = 22
+# B2 = 23
+# BPWM = 13
+
+# DIST_SP = 400   # distance setpoint
+# KP = 0.0005     # directional proprotional constant
+# KI = 0.00001    # integral (steady state error) constant
+# SPEED = 0.6     # bot speed
+
+# d = Driver((A1, A2, APWM), (B1, B2, BPWM))
+
+# # d.set_effort(SPEED)
+# # d.set_theta_effort(0.5)
+
+# dists = [0, 0, 0]
+
+# ss = 0          # steady state error correction
+
+# # Main loop will read the range and print it every second.
+# while True:
+#     print("Range: {0}mm".format(vl53.range))
+#     time.sleep(1.0)
 
 import time
 
